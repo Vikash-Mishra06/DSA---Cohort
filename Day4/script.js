9febb1ffd11ebae2100f98a3a6055a7d294231b7
Vikash Mishra <mishravikash0612@gmail.com>
2025-12-30 15:45:27 +0530

    Practiced 5 questions: 1) Sum of Two Integers, 2) Greet user, 3) Swap Two Numbers, 4) Swap Two numbers without third variable, 5) Generate Otp.

---
+++ Day4/script.js
@@ -0,0 +1,60 @@
+// Q.1 Sum of Two Integers.
+
+    // function sumOfTwoIntegers(a, b) {
+    //   let sum = a + b;
+    //   return sum;
+    // }
+
+    // let result = sumOfTwoIntegers(10, 20);
+
+    // console.log(result);
+
+    // Output: 30
+
+// -----------------------------------------------------------------------------------
+
+// Q.2 Greet user.
+
+    // function greet(name, age) {
+    //   console.log("Hello " + name + " you are " + age + " years old.");
+    // }
+
+    // greet('Vikash', 21);
+
+    // Output: Hello Vikash you are 21 years old.
+
+// -----------------------------------------------------------------------------------
+
+// Q.3 Swap Two Numbers.
+
+    // let a = Number(prompt("Enter first number."));
+    // let b = Number(prompt("Enter second number."));
+
+    // let temp = a;
+    // a = b;
+    // b = temp;
+
+    // console.log(a, b);
+
+    // Output: 20 10
+
+// -----------------------------------------------------------------------------------
+
+// Q.4 Swap Two numbers without third variable.
+
+    // let a = Number(prompt('Enter first number'));
+    // let b = Number(prompt('Enter second number'));
+
+    // let swap = [a, b] = [b, a];
+
+    // console.log(swap)
+
+    // Output: 20 10
+
+// -----------------------------------------------------------------------------------
+
+// Q.5 Generate Otp.
+
+    // console.log(Math.floor(Math.random()*9000 + 1000));
+
+// -----------------------------------------------------------------------------------
